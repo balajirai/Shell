12788abe256c2444596e6b7044f53a8d770d396d
Kali Linux <kali@rai.com>
2024-11-01 20:58:46 +0530

    mkdir with -p

---
+++ mkdir.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir -p dir1       # This will create a directory. Also if directory already exits then, just move on (no error, no override)
+# mkdir dir1          # This will create a directory. If directory already exits then it throws error that it already exits
